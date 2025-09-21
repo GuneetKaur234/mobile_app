45ecccceccd16207923c41a50efa0a1018d00a8e
GuneetKaur234 <107616806+GuneetKaur234@users.noreply.github.com>
2025-09-20 20:42:26 -0400

    Update manage.py

--- driver_backend/manage.py
+++ driver_backend/manage.py
@@ -1,22 +1,66 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+name: Build and deploy Python app to Azure Web App - Mobile-app
 
+on:
+  push:
+    branches:
+      - main
+  workflow_dispatch:
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    permissions:
+      contents: read # Required for actions/checkout
 
+    steps:
+      - uses: actions/checkout@v4
 
-if __name__ == '__main__':
-    main()
+      - name: Set up Python version
+        uses: actions/setup-python@v5
+        with:
+          python-version: '3.11'
+
+      - name: Create and start virtual environment
+        run: |
+          python -m venv venv
+          source venv/bin/activate
+
+      - name: Install dependencies
+        run: pip install -r mobile_app/driver_backend/requirements.txt
+
+      - name: Collect static files
+        run: |
+          cd mobile_app/driver_backend
+          python manage.py collectstatic --noinput
+
+      # Optional: Run Django tests
+      # - name: Run tests
+      #   run: |
+      #     cd mobile_app/driver_backend
+      #     python manage.py test
+
+      - name: Upload artifact for deployment jobs
+        uses: actions/upload-artifact@v4
+        with:
+          name: python-app
+          path: |
+            .
+            !venv/
+
+  deploy:
+    runs-on: ubuntu-latest
+    needs: build
+    
+    steps:
+      - name: Download artifact from build job
+        uses: actions/download-artifact@v4
+        with:
+          name: python-app
+      
+      - name: 'Deploy to Azure Web App'
+        uses: azure/webapps-deploy@v3
+        id: deploy-to-webapp
+        with:
+          app-name: 'Mobile-app'
+          slot-name: 'Production'
+          publish-profile: ${{ secrets.AZUREAPPSERVICE_PUBLISHPROFILE }}
