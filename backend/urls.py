7c4358d9d912ba3bce7085b3fa11f5f4fb9493c2
GuneetKaur234 <107616806+GuneetKaur234@users.noreply.github.com>
2025-09-24 18:35:07 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -2,19 +2,17 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse  # <-- add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # All driver-related API routes go through driver.urls
+    # All driver-related API routes
     path('api/driver/', include('driver.urls')),
 
     path('', lambda request: HttpResponse("Hello, Django is running!")),
-
-    
 ]
 
 # Serve media files in development
 if settings.DEBUG:
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
