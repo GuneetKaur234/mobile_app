d600297f5354a7d9db59a4de141aa8fdfea3c78c
GuneetKaur234 <107616806+GuneetKaur234@users.noreply.github.com>
2025-11-05 21:56:57 -0500

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,10 @@
-#from .celery import app as celery_app
+from .celery import app as celery_app
 
-#__all__ = ['celery_app']
+__all__ = ('celery_app',)
 
+# ⚠️ Import Django only after Celery setup
 from django.core.files.storage import storages
 from backend.azure_storage import AzureMediaStorage
 
+# Replace the default file storage with Azure Media Storage
 storages._storages['default'] = AzureMediaStorage()
